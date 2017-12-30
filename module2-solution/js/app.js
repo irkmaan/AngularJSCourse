ad4f08a7cf8e2d5e53c5fb2a670b898567a1624f
irkmaan <ccjpotter000@gmail.com>
2017-12-29 23:51:08 -0500

    Update app.js

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -2,7 +2,7 @@
     'use strict';
     
     angular.module( 'LunchCheck', [] ).controller( 'LunchCheckController', function ( $scope ) {
-        $scope.lunchMenu = 'lunch';
+        $scope.lunchMenu = '';
         $scope.checkLunch = function() {
             var menuItemCount = $scope.lunchMenu.split( ',' ).length;
             if ( menuItemCount > 3 ) {

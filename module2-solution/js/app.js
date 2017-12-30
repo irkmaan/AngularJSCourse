8f55cb9c78dcbe125e7e0941d46eb275785698c7
irkmaan <ccjpotter000@gmail.com>
2017-12-29 23:40:19 -0500

    Update app.js

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -2,6 +2,17 @@
     'use strict';
     
     angular.module( 'LunchCheck', [] ).controller( 'LunchCheckController', function ( $scope ) {
-    
+        $scope.checkLunch = function() {
+            var menuItemCount = $scope.lunchMenu.split( ',' ).length;
+            if ( menuItemCount > 3 ) {
+                $scope.lunchMessage = "Too much!";
+            }
+            else if ( $scope.lunchMenu ) {
+                $scope.lunchMessage = "Enjoy!";
+            }
+            else {
+                $scope.lunchMessage = "Please enter data first";
+            }
+        };
     } );
 } )();
